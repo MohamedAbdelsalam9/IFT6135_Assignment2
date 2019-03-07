34f128748bb4da2b950d8fc557ea9fb52d72423b
MohamedAbdelsalam9 <mohamedashraf9412@gmail.com>
2019-03-06 20:23:58 -0500

    print norm of attention

--- models.py
+++ models.py
@@ -299,6 +299,7 @@ class MultiHeadedAttention(nn.Module):
         heads = torch.matmul(a_i, value_i)
         heads = heads.transpose(1, 2).reshape((batch_size, seq_len, -1))
         a = self.linear_o(heads)
+        print(a.norm(dim=2).mean())
         a = self.dropout(a)
         return a    # size: (batch_size, seq_len, self.n_units)
 
