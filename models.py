a903221e538acbfbe40d92d179a079dcad084c45
MohamedAbdelsalam9 <mohamedashraf9412@gmail.com>
2019-03-24 04:04:46 -0400

    Remove Bias

--- models.py
+++ models.py
@@ -468,9 +468,9 @@ class MultiHeadedAttention(nn.Module):
         query_i = self.linear_q(query).reshape(batch_size, seq_len, self.n_heads, self.d_k).permute(2, 0, 1, 3)
         key_i = self.linear_k(key).reshape(batch_size, seq_len, self.n_heads, self.d_k).permute(2, 0, 1, 3)
         value_i = self.linear_v(value).reshape(batch_size, seq_len, self.n_heads, self.d_k).permute(2, 0, 1, 3)
-        query_i = query_i + self.bias_k
-        key_i = key_i + self.bias_k
-        value_i = value_i + self.bias_v
+        #query_i = query_i + self.bias_q
+        #key_i = key_i + self.bias_k
+        #value_i = value_i + self.bias_v
 
         a_i = torch.matmul(query_i, key_i.transpose(-2, -1)) / math.sqrt(self.d_k)
         mask = mask.unsqueeze(0)
