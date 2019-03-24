f210a13b488859ec10e2004e9bb8f48ae8c33b43
MohamedAbdelsalam9 <mohamedashraf9412@gmail.com>
2019-03-23 21:23:37 -0400

    Change default parameters

--- ptb-lm.py
+++ ptb-lm.py
@@ -60,7 +60,7 @@
 #          --model=TRANSFORMER --optimizer=ADAM --initial_lr=0.001 --batch_size=128 --seq_len=35 --hidden_size=512 --num_layers=2 --dp_keep_prob=.9
 #    - For Problem 4.3 (exloration of hyperparameters), do your best to get 
 #      better validation perplexities than the settings given for 4.1. You may 
-#      try any combination of the hyperparameters included as arguments in this 
+#      try any combination of the hyperparameters included as arguments in this
 #      script's ArgumentParser, but do not implement any additional 
 #      regularizers/features. You may (and will probably want to) run a lot of 
 #      different things for just 1-5 epochs when you are trying things out, but 
@@ -114,16 +114,16 @@ parser.add_argument('--optimizer', type=str, default='SGD_LR_SCHEDULE',
                     help='optimization algo to use; SGD, SGD_LR_SCHEDULE, ADAM')
 parser.add_argument('--seq_len', type=int, default=35,
                     help='number of timesteps over which BPTT is performed')
-parser.add_argument('--batch_size', type=int, default=20,
+parser.add_argument('--batch_size', type=int, default=128,
                     help='size of one minibatch')
 parser.add_argument('--initial_lr', type=float, default=20.0,
                     help='initial learning rate')
-parser.add_argument('--hidden_size', type=int, default=200,
+parser.add_argument('--hidden_size', type=int, default=512,
                     help='size of hidden layers. IMPORTANT: for the transformer\
                     this must be a multiple of 16.')
 parser.add_argument('--save_best', action='store_true',
                     help='save the model for the best validation performance')
-parser.add_argument('--num_layers', type=int, default=2,
+parser.add_argument('--num_layers', type=int, default=6,
                     help='number of hidden layers in RNN/GRU, or number of transformer blocks in TRANSFORMER')
 
 # Other hyperparameters you may want to tune in your exploration
@@ -131,7 +131,7 @@ parser.add_argument('--emb_size', type=int, default=200,
                     help='size of word embeddings')
 parser.add_argument('--num_epochs', type=int, default=40,
                     help='number of epochs to stop after')
-parser.add_argument('--dp_keep_prob', type=float, default=0.35,
+parser.add_argument('--dp_keep_prob', type=float, default=0.9,
                     help='dropout *keep* probability. drop_prob = 1-dp_keep_prob \
                     (dp_keep_prob=1 means no dropout)')
 
@@ -148,7 +148,7 @@ parser.add_argument('--evaluate', action='store_true',
                     completed ALL hyperparameter tuning on the validation set.\
                     Note we are not requiring you to do this.")
 
-# DO NOT CHANGE THIS (setting the random seed makes experiments deterministic, 
+# DO NOT CHANGE THIS (setting the random seed makes experiments deterministic,
 # which helps for reproducibility)
 parser.add_argument('--seed', type=int, default=1111,
                     help='random seed')
@@ -208,7 +208,7 @@ else:
 # HELPER FUNCTIONS
 def _read_words(filename):
     with open(filename, "r") as f:
-      return f.read().replace("\n", "<eos>").split()
+        return f.read().replace("\n", "<eos>").split()
 
 def _build_vocab(filename):
     data = _read_words(filename)
