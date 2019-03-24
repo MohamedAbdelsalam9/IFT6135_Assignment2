92f9aed1903fb2f5dfa2c128091ccb6a29257c1d
MohamedAbdelsalam9 <mohamedashraf9412@gmail.com>
2019-03-23 23:27:16 -0400

    Try another implementation (Transformer)

--- models.py
+++ models.py
@@ -462,17 +462,17 @@ class MultiHeadedAttention(nn.Module):
         batch_size = query.shape[0]
         seq_len = query.shape[1]
 
-        query_i = self.linear_q(query).view(batch_size, seq_len, self.n_heads, self.d_k).permute(0, 2, 1, 3)#.contiguous().view(-1,seq_len,self.d_k)
-        key_i = self.linear_k(key).view(batch_size, seq_len, self.n_heads, self.d_k).permute(0, 2, 1, 3)#.contiguous().view(-1,seq_len,self.d_k)
-        value_i = self.linear_v(value).view(batch_size, seq_len, self.n_heads, self.d_k).permute(0, 2, 1, 3)#.contiguous().view(-1,seq_len,self.d_k)
+        query_i = self.linear_q(query).view(batch_size, seq_len, self.n_heads, self.d_k).permute(2, 0, 1, 3)#.contiguous().view(-1,seq_len,self.d_k)
+        key_i = self.linear_k(key).view(batch_size, seq_len, self.n_heads, self.d_k).permute(2, 0, 1, 3)#.contiguous().view(-1,seq_len,self.d_k)
+        value_i = self.linear_v(value).view(batch_size, seq_len, self.n_heads, self.d_k).permute(2, 0, 1, 3)#.contiguous().view(-1,seq_len,self.d_k)
 
         a_i = torch.matmul(query_i, key_i.transpose(-2, -1)) / math.sqrt(self.d_k)
-        mask = mask.unsqueeze(1).repeat(1,self.n_heads,1,1)#.reshape(batch_size, self.n_heads, seq_len, seq_len)
+        mask = mask.unsqueeze(0).repeat(self.n_heads,1,1,1)#.reshape(batch_size, self.n_heads, seq_len, seq_len)
         a_i = a_i * mask - 10**9 * (1 - mask)
         a_i = torch.exp(a_i - a_i.max(dim=-1)[0].unsqueeze(-1))
         a_i = a_i / torch.sum(a_i, -1, keepdim=True)
         heads = torch.matmul(a_i, value_i)
-        heads = heads.transpose(1, 2).reshape((batch_size, seq_len, -1))
+        heads = heads.permute(1, 2, 0, 3).reshape((batch_size, seq_len, -1))
         a = self.linear_o(heads)
         a = self.dropout(a)
         return a    # size: (batch_size, seq_len, self.n_units)
